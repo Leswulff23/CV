c01e4da2f9dc20f719729f516d232323479f2f13
Leslie Kodjoe <leslie.kodjoe@ashesi.edu.gh>
2020-09-22 22:18:41 +0000

    Updates

--- CV.js
+++ CV.js
@@ -1,8 +1,7 @@
 window.addEventListener('load', () => {
 
     document.getElementById("result-fname").innerHTML= localStorage.getItem("FName");
-    document.getElementById("result-mname").innerHTML=localStorage.getItem("MName");
-    document.getElementById("result-lname").innerHTML=localStorage.getItem("LName");
+    document.getElementById("result-mlname").innerHTML=localStorage.getItem("MName")+" "+localStorage.getItem("LName");
     document.getElementById("result-gender").innerHTML=localStorage.getItem("Gender");
     document.getElementById("result-phone").innerHTML=localStorage.getItem("Phone");
     document.getElementById("result-email").innerHTML=localStorage.getItem("Email");
